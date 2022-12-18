984642f749393aa64bda71df5125a1a3492e7e16
Darwen <75426347+thisisdarwen@users.noreply.github.com>
2022-12-18 07:45:23 +0300

    Chat guard botu

---
+++ src/config.js
@@ -0,0 +1,14 @@
+const config = { 
+//------------Connections-----------//
+    Client_Token: 'OTc1NDMzOTMwMDE2NjkwMTc2.G31YYL.RZI_m41elB8xoBIt1aJd8GHxVXOv-pFRP5nQGU',
+    MongoDB_ConnectURL: 'mongodb+srv://LWZcode:1234onan@cluster0.kjske.mongodb.net/LWZcode?retryWrites=true&w=majority',
+    //------------BotSettings-----------//
+    Prefix: '.',
+    BotOwners: ["947911186735841311"],
+    Custom_Status_Text: 'Lowzy ❤️ Smarty',
+    Custom_Status_Type: 'PLAYING', // => PLAYING / WATCHING / LISTENING
+    Custom_Status: 'Lowzy', // => dnd / idle / online / invisible
+    VoiceChannelID: '971473785020637285'
+};
+  
+module.exports = config;
